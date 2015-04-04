7f0207d34f980a1beae3eac73d7f8c06a7cc91e4
Alex Baker <alex@alexbaker.io>
2015-04-03 23:02:07 -0600

    simplified whitspace stripping and flattened logic

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,15 +1,10 @@
-#
-# Python "Bob" exercise.
-#
-
-
 def hey(what):
-    what_strip = " ".join(what.split())
-    if what_strip:
-        if what_strip.isupper():
-            return "Whoa, chill out!"
-        elif what_strip[-1] == '?':
-            return "Sure."
-        else:
-            return "Whatever."
-    return "Fine. Be that way!"
+    what_strip = what.strip()
+    if not what_strip:
+        return "Fine. Be that way!"
+    elif what_strip.isupper():
+        return "Whoa, chill out!"
+    elif what_strip[-1] == '?':
+        return "Sure."
+    else:
+        return "Whatever."
