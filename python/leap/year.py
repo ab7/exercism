b77691f12dbfe29da7350a0d9ea380d2ef9d9a18
Alex Baker <alex@alexbaker.io>
2015-04-04 09:31:12 -0600

    removed unnecessary logic and refactored to single if statement

--- python/leap/year.py
+++ python/leap/year.py
@@ -1,6 +1,4 @@
 def is_leap_year(year):
-    if year % 400 == 0 and year % 100 == 0:
-        return True
-    elif year % 4 == 0 and year % 100 != 0:
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
         return True
     return False
