8efd9e1e398197179568835ed185632dd9922b6c
Alex Baker <alex@alexbaker.io>
2015-04-19 13:50:31 -0600

    refactored to builtin strtr

--- php/rna-transcription/rna-transcription.php
+++ php/rna-transcription/rna-transcription.php
@@ -2,19 +2,5 @@
 
 function toRna($strand)
 {
-    $transcription = '';
-    for ($i = 0; $i < strlen($strand); $i++) {
-        if ($strand[$i] === 'G') {
-            $transcription .= 'C';
-        } elseif ($strand[$i] === 'C') {
-            $transcription .= 'G';
-        } elseif ($strand[$i] === 'T') {
-            $transcription .= 'A';
-        } elseif ($strand[$i] === 'A') {
-            $transcription .= 'U';
-        } else {
-            throw new UnexpectedValueException("$strand[$i] is an invalid nucleotide!");
-        }
-    }
-    return $transcription;
+    return strtr($strand, 'GCTA', 'CGAU');
 }
