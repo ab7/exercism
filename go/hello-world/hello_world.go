9595e23fc698f217317bc66c7b3e735665b59380
Alex Baker <hello@alexbaker.io>
2020-04-16 19:12:34 -0600

    Add solution for hello-world go exercise

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting implements the classic "Hello, World!" program.
+package greeting
+
+// HelloWorld returns a friendly greeting to the World.
+func HelloWorld() string {
+    return "Hello, World!"
+}
