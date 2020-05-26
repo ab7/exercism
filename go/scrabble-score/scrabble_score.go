08829585d84abd06524516db1de38a3e5534c6f0
Alex Baker <hello@alexbaker.io>
2020-05-26 03:34:05 -0600

    Add solution for scrabble-score go exercise

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,41 @@
+/*
+Package scrabble implements a library for processing scrabble scores.
+
+The scoring of scrabble letters is as follows:
+
+Letter                             Value
+A, E, I, O, U, L, N, R, S, T       1
+D, G                               2
+B, C, M, P                         3
+F, H, V, W, Y                      4
+K                                  5
+J, X                               8
+Q, Z                               10
+*/
+package scrabble
+
+import "strings"
+
+// Score calculates the scrabble score for a given string.
+func Score(s string) int {
+	score := 0
+	for _, c := range strings.ToLower(s) {
+		switch c {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			score++
+		case 'd', 'g':
+			score += 2
+		case 'b', 'c', 'm', 'p':
+			score += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			score += 4
+		case 'k':
+			score += 5
+		case 'j', 'x':
+			score += 8
+		case 'q', 'z':
+			score += 10
+		}
+	}
+	return score
+}
