28e7326fb79df7483c53584c1c75edae13b3259d
Alex Baker <hello@alexbaker.io>
2020-06-20 12:04:31 -0600

    Solve python reverse-string using mirror

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -1,5 +1,21 @@
-def reverse(text: str) -> str:
+def reverse_using_builtin(text: str) -> str:
     """
     Return reversed string using Python builtin.
     """
     return text[::-1]
+
+
+def reverse(text: str) -> str:
+    """
+    Return reversed string without magic methods using mirror.
+    """
+    text_list = [c for c in text]
+    start = 0
+    end = len(text) - 1
+    while start < end:
+        start_saved = text_list[start]
+        text_list[start] = text_list[end]
+        text_list[end] = start_saved
+        start += 1
+        end -= 1
+    return ''.join(text_list)
