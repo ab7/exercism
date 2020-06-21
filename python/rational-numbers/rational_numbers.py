a307fe44e9d8bdef77ca1ef9d7b24e47df4f2bbe
Alex Baker <hello@alexbaker.io>
2020-06-21 12:39:40 -0600

    Return new instance of class in magic methods

--- python/rational-numbers/rational_numbers.py
+++ python/rational-numbers/rational_numbers.py
@@ -3,9 +3,9 @@ from fractions import Fraction
 
 class Rational:
     def __init__(self, numer, denom):
+        numer, denom = self._reduce_fraction(numer, denom)
         self.numer = numer
         self.denom = denom
-        self._reduce_fraction(self.numer, self.denom)
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
@@ -16,43 +16,37 @@ class Rational:
     def __add__(self, other):
         numer = self.numer * other.denom + other.numer * self.denom
         denom = self.denom * other.denom
-        self._reduce_fraction(numer, denom)
-        return self
+        return self.__class__(*self._reduce_fraction(numer, denom))
 
     def __sub__(self, other):
         numer = self.numer * other.denom - other.numer * self.denom
         denom = self.denom * other.denom
-        self._reduce_fraction(numer, denom)
-        return self
+        return self.__class__(*self._reduce_fraction(numer, denom))
 
     def __mul__(self, other):
         numer = self.numer * other.numer
         denom = self.denom * other.denom
-        self._reduce_fraction(numer, denom)
-        return self
+        return self.__class__(*self._reduce_fraction(numer, denom))
 
     def __truediv__(self, other):
         numer = self.numer * other.denom
         denom = self.denom * other.numer
-        self._reduce_fraction(numer, denom)
-        return self
+        return self.__class__(*self._reduce_fraction(numer, denom))
 
     def __abs__(self):
         numer = abs(self.numer)
         denom = abs(self.denom)
-        self._reduce_fraction(numer, denom)
-        return self
+        return self.__class__(*self._reduce_fraction(numer, denom))
 
     def __pow__(self, power):
         numer = self.numer ** abs(power)
         denom = self.denom ** abs(power)
-        self._reduce_fraction(numer, denom)
-        return self
+        return self.__class__(*self._reduce_fraction(numer, denom))
 
     def __rpow__(self, base):
         return base ** (self.numer / self.denom)
 
-    def _reduce_fraction(self, numer, denom):
+    @staticmethod
+    def _reduce_fraction(numer, denom):
         reduced = Fraction(numer, denom)
-        self.numer = reduced.numerator
-        self.denom = reduced.denominator
+        return reduced.numerator, reduced.denominator
