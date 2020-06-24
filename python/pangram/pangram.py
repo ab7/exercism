a7bbe96ca390b94f9f013b7e824376216183b294
Alex Baker <hello@alexbaker.io>
2020-06-23 21:06:01 -0600

    Inital solution for python pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,7 @@
-def is_pangram(sentence):
-    pass
+def is_pangram(sentence: str) -> bool:
+    char_set = set()
+    for char in sentence:
+        ascii_val = ord(char.lower())
+        if ord('a') <= ascii_val <= ord('z'):
+            char_set.add(ascii_val)
+    return len(char_set) == 26
