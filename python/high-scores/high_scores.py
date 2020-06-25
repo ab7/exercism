270d85d76f01c7b4e7e4815ee9cb3ab595208142
Alex Baker <hello@alexbaker.io>
2020-06-24 20:38:32 -0600

    Initial solution for python high-scores

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,25 @@
-def latest(scores):
-    pass
+def latest(scores: list) -> int:
+    """
+    Return the last added score from collection.
+    """
+    return scores[-1]
 
 
-def personal_best(scores):
-    pass
+def personal_best(scores: list) -> int:
+    """
+    Return max score from score collection.
+    """
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    """
+    Return top three scores from score collection.
+    """
+    top_three = scores[:3]
+    for score in scores[3:]:
+        for i, top in enumerate(top_three):
+            if score > top:
+                top_three[i] = score
+                break
+    return sorted(top_three, reverse=True)
