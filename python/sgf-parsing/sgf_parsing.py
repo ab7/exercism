b0a6feb784cff66576e1a111d0c510b8c4ac9552
Alex Baker <hello@alexbaker.io>
2020-06-24 19:59:29 -0600

    Initial solution for python sgf-parsing

--- python/sgf-parsing/sgf_parsing.py
+++ python/sgf-parsing/sgf_parsing.py
@@ -25,5 +25,33 @@ class SgfTree:
         return not self == other
 
 
-def parse(input_string):
-    pass
+def parse(input_string: str) -> SgfTree:
+    """
+    Examples:
+    (;FF[4]C[root]SZ[19];B[aa];W[ab])
+    ['(', 'FF[4]C[root]SZ[19]', 'B[aa]', 'W[ab])']
+    node 3 props FF[4] c[root] SZ[19]
+      - child node 1 prop B[aa]
+        - child node 1 prop W[ab]
+
+    (;FF[4](;B[aa];W[ab])(;B[dd];W[ee]))
+    ['(', 'FF[4](', 'B[aa]', 'W[ab])(', 'B[dd]', 'W[ee]))']
+
+    (;FF[4];AB[aa][ab][ba])
+    ['(', 'FF[4]', 'AB[aa][ab][ba])']
+    """
+    nodes = input_string[1:-1].split(';')
+    last_node = None
+    while len(nodes) > 0:
+        node_str = nodes.pop()
+        if not node_str:
+            continue
+        new_node = SgfTree()
+        if last_node:
+            new_node.children.append(last_node)
+        prop_strs = node_str.split('[')
+        new_node.properties[prop_strs[0]] = []
+        for prop in prop_strs[1:]:
+            new_node.properties[prop_strs[0]].append(prop[:-1])
+        last_node = new_node
+    return last_node
