acd7dfd94afafd453b30d06b77bbf17d027f7499
Alex Baker <hello@alexbaker.io>
2020-06-25 19:51:52 -0600

    Add initial solution for python matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,28 @@
+from typing import List
+
+
 class Matrix:
-    def __init__(self, matrix_string):
-        pass
+    """Matrix implements utiliy methods for parsing a matrix string.
 
-    def row(self, index):
-        pass
+    The initialized matrix string is assumed to have rows delimited by the Unix
+    style newline character and columns delimited by the space character.
+    """
 
-    def column(self, index):
-        pass
+    Row = List[int]
+    Rows = List[Row]
+    Column = List[int]
+
+    def __init__(self, matrix_string: str) -> None:
+        self.matrix_string = matrix_string
+
+    def _converted_matrix(self) -> Rows:
+        """Convert a matrix string to a list using provided delimiters."""
+        return [[int(c) for c in r.split()] for r in self.matrix_string.split('\n')]
+
+    def row(self, index: int) -> Row:
+        """Return a Row by index using converted matrix string."""
+        return self._converted_matrix()[index - 1]
+
+    def column(self, index: int) -> Column:
+        """Return a Column by index using converted matrix string."""
+        return [row[index - 1] for row in self._converted_matrix()]
