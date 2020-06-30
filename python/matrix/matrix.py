9c48101f0ef53c6453b9a1c4e54ad8f3bf78ed1c
Alex Baker <hello@alexbaker.io>
2020-06-30 12:34:41 -0400

    Refactor python matrix getters

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -5,27 +5,14 @@ class Matrix:
     style newline character and columns delimited by the space character.
     """
 
-    def __init__(self, matrix_string: str,
-                 row_delimiter: str = ' ',
-                 col_delimiter: str = '\n') -> None:
-        self.row_delimiter = row_delimiter
-        self.col_delimiter = col_delimiter
-        self.rows = self.matrix_string.split(self.row_delimiter)
-
-    @staticmethod
-    def convert_list_type(items: list, item_type: type) -> list:
-        """Convert a list of items to provided type."""
-        return [item_type(item) for item in items]
+    def __init__(self, matrix_string: str) -> None:
+        rows = matrix_string.splitlines()
+        self.matrix = [[int(num) for num in row.split()] for row in rows]
 
     def row(self, index: int) -> list:
         """Return row as list of integers at given index."""
-        row = self.rows[index - 1].split(self.col_delimiter)
-        return self.convert_list_type(row, int)
+        return self.matrix[index - 1]
 
     def column(self, index: int) -> list:
         """Return column as list of integers at given index."""
-        column_list = []
-        for row in self.rows:
-            row = row.split(self.col_delimiter)
-            column_list.append(row[index - 1])
-        return self.convert_list_type(column_list, int)
+        return [row[index - 1] for row in self.matrix]
