46f254a9a86070ba4f00d101f5a9d4a46682e40a
Alex Baker <hello@alexbaker.io>
2020-07-01 10:18:52 -0400

    Add initial solution for python hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,11 @@
-def distance(strand_a, strand_b):
-    pass
+def distance(strand_a: str, strand_b: str) -> int:
+    """
+    Calculate the Hamming Distance between two DNA strands.
+    """
+    if len(strand_a) != len(strand_b):
+        raise ValueError('DNA strand lengths do not match.')
+    ham = 0
+    for i in range(len(strand_a)):
+        if strand_a[i].lower() != strand_b[i].lower():
+            ham += 1
+    return ham
