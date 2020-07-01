74f10f9a44d61a80bdc16b24b0289a7947ad04c5
Alex Baker <hello@alexbaker.io>
2020-07-01 18:02:40 -0400

    Add initial solution for python armstrong-numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,13 @@
-def is_armstrong_number(number):
-    pass
+def is_armstrong_number(number: int) -> bool:
+    """Determine if given integer is an Armstrong number.
+
+    An Armstrong number is a number that is the sum of its
+    own digits each raised to the power of the number of digits.
+
+    See https://en.wikipedia.org/wiki/Narcissistic_number.
+    """
+    result = 0
+    num_str = str(number)
+    for i in num_str:
+        result += int(i) ** len(num_str)
+    return result == number
