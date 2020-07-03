e772930b3d78da3a850d669c710d45873dfb35ee
Alex Baker <hello@alexbaker.io>
2020-07-03 08:39:52 -0400

    Add initial solution for python anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,10 @@
 def find_anagrams(word, candidates):
-    pass
+    word_chars = sorted([char.lower() for char in word])
+    anagrams = []
+    for _word in candidates:
+        if _word.lower() == word.lower():
+            continue
+        chars = sorted([char.lower() for char in _word])
+        if chars == word_chars:
+            anagrams.append(_word)
+    return anagrams
