6a6556ca34b4e3de936baa60a5a32ecd15463c0e
Alex Baker <hello@alexbaker.io>
2020-07-02 21:22:40 -0400

    Use Faulhaber's formula to calculate sums

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,19 @@
 def square_of_sum(number: int) -> int:
-    return sum(range(1, number + 1)) ** 2
+    """Return sum of first N natural numbers squared.
+
+    This algorithm is based on Faulhaber's formula.
+    """
+    return ((number * (number + 1)) / 2) ** 2
 
 
 def sum_of_squares(number: int) -> int:
-    return sum([i ** 2 for i in range(1, number + 1)])
+    """Return sum of the squares of the first N natural numbers.
+
+    This algorithm is based on Faulhaber's formula.
+    """
+    return (number * (number + 1) * (2 * number + 1)) / 6
 
 
 def difference_of_squares(number: int) -> int:
+    """Return the difference bewteen square of the sum and the sum of the squares."""
     return square_of_sum(number) - sum_of_squares(number)
