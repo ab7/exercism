9d8bb86a7c6599dda50d78081c9c3ea55160f79f
Alex Baker <hello@alexbaker.io>
2020-07-03 07:30:36 -0400

    Refactor python isbn-verifier

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -4,22 +4,21 @@ def is_valid(isbn: str) -> bool:
 
     A ISBN-10 number has exactly 9 digits plus a check number.
     The check number can be a digit [0-9] or the character 'X' which
-    represents 10. To check validity each digit is multiplied to the
+    represents 10. To check validity each digit is multiplied by the
     count and summed with the previous digit. The remainder of the
     total sum of the 10 products divided by 11 should be 0.
 
     Dashes are valid as part of the ISBN and are ignored.
     """
     total = 0
-    mult = 10
+    count = 10
     for char in isbn:
-        if ord('0') <= ord(char) <= ord('9'):
-            total += int(char) * mult
-        elif mult == 1 and char.lower() == 'x':
-            total += 10 * mult
+        if char.isdigit():
+            total += int(char) * count
+            count -= 1
+        elif count == 1 and char.lower() == 'x':
+            total += 10 * count
+            count -= 1
         elif char != '-':
             return False
-        else:
-            continue
-        mult -= 1
-    return mult == 0 and total % 11 == 0
+    return count == 0 and total % 11 == 0
