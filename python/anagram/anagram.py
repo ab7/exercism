11abe51da6cae3672358f109b155938326c0c4c5
Alex Baker <hello@alexbaker.io>
2020-07-05 11:44:10 -0400

    Refactor python anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,10 +1,3 @@
-def find_anagrams(word, candidates):
-    word_chars = sorted([char.lower() for char in word])
-    anagrams = []
-    for _word in candidates:
-        if _word.lower() == word.lower():
-            continue
-        chars = sorted([char.lower() for char in _word])
-        if chars == word_chars:
-            anagrams.append(_word)
-    return anagrams
+def find_anagrams(word: str, candidates: list) -> list:
+    filtered = [w for w in candidates if w.lower() != word.lower()]
+    return [w for w in filtered if sorted(word.lower()) == sorted(w.lower())]
