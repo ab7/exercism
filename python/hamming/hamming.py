6393bbe79972709f5c2b3c9d3b54c55b4536f2b7
Alex Baker <hello@alexbaker.io>
2020-07-05 11:08:24 -0400

    Sum booleans instead of ints

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -5,4 +5,4 @@ def distance(strand_a: str, strand_b: str) -> int:
     if len(strand_a) != len(strand_b):
         raise ValueError('DNA strand lengths do not match.')
     pairs = zip(strand_a.lower(), strand_b.lower())
-    return sum([1 for pair in pairs if pair[0] != pair[1]])
+    return sum([ch1 != ch2 for ch1, ch2 in pairs])
