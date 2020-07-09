24277562697cfe1bcbd6ad3738e7f30bd4991122
Alex Baker <hello@alexbaker.io>
2020-07-08 21:50:45 -0400

    Add initial solution for python series

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,4 @@
-def slices(series, length):
-    pass
+def slices(series: str, length: int) -> list:
+    if length <= 0 or length > len(series):
+        raise ValueError('Length must be between 1 and the length of the series.')
+    return [series[i:i + length] for i in range(len(series) - length + 1)]
