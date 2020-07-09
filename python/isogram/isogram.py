48cf7216a38a68bd615dc5dd4ef205832f735ad4
Alex Baker <hello@alexbaker.io>
2020-07-08 22:03:07 -0400

    Add initial solution for python isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,8 @@
-def is_isogram(string):
-    pass
+def is_isogram(string: str) -> bool:
+    letters = set()
+    for char in string.lower():
+        if char.isalpha():
+            if char in letters:
+                return False
+            letters.add(char)
+    return True
