e468d056f22cb390747a8d47713f5d1dc9a96a9e
Alex Baker <hello@alexbaker.io>
2020-07-12 15:53:27 -0600

    Refactor python grains to use geometric series

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,11 +1,14 @@
 def square(number: int) -> int:
+    """Return value for number in geometric series.
+
+    The input must be a valid integer representing a square on a chess board.
+    There are 64 squares on a chess board.
+    """
     if number < 1 or number > 64:
         raise ValueError('Input does not represent a valid chess square.')
-    result = 1
-    for _ in range(0, number - 1):
-        result *= 2
-    return result
+    return 2 ** (number - 1)
 
 
 def total() -> int:
+    """Return geometric series sum based on number of chess squares (64)."""
     return sum(square(i) for i in range(1, 65))
