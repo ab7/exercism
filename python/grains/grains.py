0a4b553299e1e5408cb39efd6decd0433a55b3e9
Alex Baker <hello@alexbaker.io>
2020-07-12 15:42:49 -0600

    Initial brute force for python grains

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,11 @@
-def square(number):
-    pass
+def square(number: int) -> int:
+    if number < 1 or number > 64:
+        raise ValueError('Input does not represent a valid chess square.')
+    result = 1
+    for _ in range(0, number - 1):
+        result *= 2
+    return result
 
 
-def total():
-    pass
+def total() -> int:
+    return sum(square(i) for i in range(1, 65))
