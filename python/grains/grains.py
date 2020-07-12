c8cb6edaa272d81cd91b593faba7f1d92ed08c11
Alex Baker <hello@alexbaker.io>
2020-07-12 16:19:19 -0600

    Use left shift to calculate geometric series

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -6,9 +6,9 @@ def square(number: int) -> int:
     """
     if number < 1 or number > 64:
         raise ValueError('Input does not represent a valid chess square.')
-    return 2 ** (number - 1)
+    return 1 << (number - 1)
 
 
 def total() -> int:
     """Return geometric series sum based on number of chess squares (64)."""
-    return sum(square(i) for i in range(1, 65))
+    return (1 << 64) - 1
