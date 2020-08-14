59c6778b0893e9427b372a4c23dae98fc100c742
Alex Baker <hello@alexbaker.io>
2020-08-14 07:36:03 -0600

    Add initial solution for python scrabble_score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,19 @@
 def score(word):
-    pass
+    result = 0
+    for char in word:
+        char_upper = char.upper()
+        if char_upper in ('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'):
+            result += 1
+        elif char_upper in ('D', 'G'):
+            result += 2
+        elif char_upper in ('B', 'C', 'M', 'P'):
+            result += 3
+        elif char_upper in ('F', 'H', 'V', 'W', 'Y'):
+            result += 4
+        elif char_upper in ('K'):
+            result += 5
+        elif char_upper in ('J', 'X'):
+            result += 8
+        elif char_upper in ('Q', 'Z'):
+            result += 10
+    return result
