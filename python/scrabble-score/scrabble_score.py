2bea178bea413263b4f45259c9d7acf513fe08d5
Alex Baker <hello@alexbaker.io>
2020-09-02 16:29:10 -0600

    Use dict and sum to process scrabble score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,19 +1,29 @@
 def score(word):
-    result = 0
-    for char in word:
-        char_upper = char.upper()
-        if char_upper in ('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'):
-            result += 1
-        elif char_upper in ('D', 'G'):
-            result += 2
-        elif char_upper in ('B', 'C', 'M', 'P'):
-            result += 3
-        elif char_upper in ('F', 'H', 'V', 'W', 'Y'):
-            result += 4
-        elif char_upper in ('K'):
-            result += 5
-        elif char_upper in ('J', 'X'):
-            result += 8
-        elif char_upper in ('Q', 'Z'):
-            result += 10
-    return result
+    return sum({
+        'A': 1,
+        'E': 1,
+        'I': 1,
+        'O': 1,
+        'U': 1,
+        'L': 1,
+        'N': 1,
+        'R': 1,
+        'S': 1,
+        'T': 1,
+        'D': 2,
+        'G': 2,
+        'B': 3,
+        'C': 3,
+        'M': 3,
+        'P': 3,
+        'F': 4,
+        'H': 4,
+        'V': 4,
+        'W': 4,
+        'Y': 4,
+        'K': 5,
+        'J': 8,
+        'X': 8,
+        'Q': 10,
+        'Z': 10,
+    }[char] for char in word.upper())
